be6a8cd78c605bb464a202c5b48caa851e235cd0
Alex Cordeiro <alex.sandro.1199@gmail.com>
2025-05-10 09:09:25 -0300

    instalacao dependencias: pg, typeorm e dotenv

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // Mude para o banco escolhido
+      host: 'localhost',
+      port: 5432, // Troque para a porta correta do seu banco
+      username: 'seu_usuario',
+      password: 'sua_senha',
+      database: 'seu_banco',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
