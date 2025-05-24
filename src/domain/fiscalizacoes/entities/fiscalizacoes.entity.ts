433df8fa03bc113cea7d4c44a9c87c972c68bc00
burgues0 <victordpp@outlook.com>
2025-05-24 11:14:03 -0300

    edit: add integration fiscalizacao x obras

--- src/domain/fiscalizacoes/entities/fiscalizacoes.entity.ts
+++ src/domain/fiscalizacoes/entities/fiscalizacoes.entity.ts
@@ -3,12 +3,13 @@ import {
     PrimaryGeneratedColumn,
     Column,
     ManyToOne,
+    ManyToMany,
     OneToMany,
     CreateDateColumn,
     UpdateDateColumn,
     JoinTable,
 } from 'typeorm';
-//import { Obra } from 'src/domain/obras/entities/obra.entity'; todo
+import { Obra } from 'src/domain/obras/entities/obra.entity';
 //import { Relatorio } from 'src/domain/relatorios/entities/relatorio.entity'; todo
 //import { ResponsavelTecnico } from 'src/domain/responsaveis-tecnicos/entities/responsavel.entity'; pendente Levy
   
@@ -17,29 +18,28 @@ export class Fiscalizacao {
     @PrimaryGeneratedColumn()
     id: number;
 
-    @Column()
+    @Column({ type: 'varchar', length: 50 })
     titulo: string;
 
-    @Column({ nullable: true })
+    @Column({ type: 'varchar', length: 255, nullable: true })
     descricao: string;
 
     @Column({ type: 'date' })
     data: Date;
 
-    // to-do
-    // @ManyToMany(() => Obra, obra => obra.fiscalizacoes)
-    // @JoinTable({
-    //     name: 'obra_fiscalizacoes',
-    //     joinColumn: {
-    //         name: 'fiscalizacoes_id',
-    //         referencedColumnName: 'id',
-    //     },
-    //     inverseJoinColumn: {
-    //         name: 'obra_id',
-    //         referencedColumnName: 'id',
-    //     }
-    // })
-    // obra: Obra[];
+    @ManyToMany(() => Obra, obra => obra.fiscalizacoes)
+    @JoinTable({
+        name: 'obra_fiscalizacoes',
+        joinColumn: {
+            name: 'fiscalizacoes_id',
+            referencedColumnName: 'id',
+        },
+        inverseJoinColumn: {
+            name: 'obra_id',
+            referencedColumnName: 'id',
+        }
+    })
+    obras: Obra[];
 
     //pendente Levy
     //@ManyToOne(() => ResponsavelTecnico, { nullable: true })
