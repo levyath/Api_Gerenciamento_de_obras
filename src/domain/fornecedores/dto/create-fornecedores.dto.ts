50719b9118b70a423bf58a62e6e5cd691ce0e02b
levyath <levyarthur2@gmail.com>
2025-05-31 12:14:44 -0300

    definição do tipo de propriedades do create-fornecedor

--- src/domain/fornecedores/dto/create-fornecedores.dto.ts
+++ src/domain/fornecedores/dto/create-fornecedores.dto.ts
@@ -1,16 +1,24 @@
 import { Type } from "class-transformer";
-import { IsArray, IsOptional, ValidateNested } from "class-validator";
-
+import { IsArray, IsOptional, ValidateNested, IsString } from "class-validator";
 
 export class CreateFornecedoresDto {
+  @IsString()
   nome: string;
 
+  @IsOptional()
+  @IsString()
   cnpj?: string;
 
+  @IsOptional()
+  @IsString()
   email?: string;
 
+  @IsOptional()
+  @IsString()
   telefone?: string;
 
+  @IsOptional()
+  @IsString()
   endereco?: string;
 
   @IsOptional()
