1515ca99ae419839500ca2e4f26af5457fea166b
levyath <levyarthur2@gmail.com>
2025-05-31 12:48:42 -0300

    removendo propriedades de dto de update obras pois ja esta sendo utilizado o PartialType

--- src/domain/obras/dto/update-obra.dto.ts
+++ src/domain/obras/dto/update-obra.dto.ts
@@ -2,10 +2,4 @@ import { PartialType } from '@nestjs/mapped-types';
 import { CreateObraDto } from './create-obra.dto';
 import { IsOptional, IsArray, IsNumber } from 'class-validator';
 
-export class UpdateObraDto extends PartialType(CreateObraDto) {
-  // TODO: remoção futura
-  // @IsOptional()
-  // @IsArray()
-  // @IsNumber({}, { each: true })
-  // fornecedoresId?: number[];
-}
+export class UpdateObraDto extends PartialType(CreateObraDto) {}
