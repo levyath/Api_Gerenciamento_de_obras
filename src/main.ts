2771500d02cb4cb35ea592c017732d67416ba7e7
levyath <levyarthur2@gmail.com>
2025-06-03 23:20:46 -0300

    ajuste no swagger, configuracoes basicas, mudanca de endpoint para documentacao do mesmo, alteracao de algumas options como explicitacao do uso de jwt e possibilidade de inseri-lo via interface do proprio swagger

--- src/main.ts
+++ src/main.ts
@@ -5,15 +5,27 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
   // Configuração do Swagger
   const config = new DocumentBuilder()
     .setTitle('API de Obras')
     .setDescription('Documentação da API para gerenciamento de obras')
     .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .addServer('http://localhost:3000', 'Development')
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('', app, document); // http://localhost:3000
+  SwaggerModule.setup('docs', app, document); // http://localhost:3000/docs
 
   await app.listen(process.env.PORT ?? 3000);
 }
